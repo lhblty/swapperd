bf835983f7705a1495003958298c9f512f1b194b
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-30 13:53:51 +1100

    adapter/server: add comments, marshallers and unmarshalers

--- adapter/server/marshal.go
+++ adapter/server/marshal.go
@@ -27,16 +27,24 @@ type GetBalancesResponse map[blockchain.TokenName]blockchain.Balance
 type GetAddressesResponse map[blockchain.TokenName]string
 type GetAddressResponse string
 
-type PostSwapRequest swap.SwapBlob
+// PostSwapsRequest is the object expected by a POST \swaps call
+type PostSwapsRequest swap.SwapBlob
 
-type PostSwapResponse struct {
+// PostSwapsResponse is the object returned on a successful POST \swaps call
+type PostSwapsResponse struct {
 	ID        swap.SwapID   `json:"id"`
 	Swap      swap.SwapBlob `json:"swap,omitempty"`
 	Signature string        `json:"signature,omitempty"`
 }
 
-type PostRedeemSwapResponse struct {
-	ID swap.SwapID `json:"id"`
+// MarshalInitiatorPostSwapsResponse marshals an initiator's PostSwapsResponse
+func MarshalInitiatorPostSwapsResponse(id swap.SwapID, swap swap.SwapBlob, signature string) PostSwapsResponse {
+	return PostSwapsResponse{id, swap, signature}
+}
+
+// MarshalResponderPostSwapsResponse marshals an responder's PostSwapsResponse
+func MarshalResponderPostSwapsResponse(id swap.SwapID) PostSwapsResponse {
+	return PostSwapsResponse{ID: id}
 }
 
 type PostTransfersRequest struct {
@@ -48,20 +56,40 @@ type PostTransfersRequest struct {
 
 type PostTransfersResponse transfer.TransferReceipt
 
+// GetSignatureResponseJSON is the object returned when the user calls GET
+// /signature?type=json
 type GetSignatureResponseJSON struct {
 	Message   json.RawMessage `json:"message"`
 	Signature string          `json:"signature"`
 }
 
+// MarshalGetSignatureResponseJSON marshals a message and signature to
+// GetSignatureResponseJSON
+func MarshalGetSignatureResponseJSON(message json.RawMessage, signature string) GetSignatureResponseJSON {
+	return GetSignatureResponseJSON{message, signature}
+}
+
+// GetSignatureResponseString is the object returned when the user calls GET
+// /signature?type=base64|hex
 type GetSignatureResponseString struct {
 	Message   string `json:"message"`
 	Signature string `json:"signature"`
 }
 
+// MarshalGetSignatureResponseString marshals a message and signature to
+// GetSignatureResponseString
+func MarshalGetSignatureResponseString(message, signature string) GetSignatureResponseString {
+	return GetSignatureResponseString{message, signature}
+}
+
+// GetTransfersResponse is the object returned when the user calls GET
+// /transfers
 type GetTransfersResponse struct {
 	Transfers []transfer.TransferReceipt `json:"transfers"`
 }
 
+// MarshalGetTransfersResponse marshals internal representation of transfer
+// receipts to GetTransfersResponse
 func MarshalGetTransfersResponse(receiptMap transfer.TransferReceiptMap) GetTransfersResponse {
 	transfers := []transfer.TransferReceipt{}
 	for _, receipt := range receiptMap {
